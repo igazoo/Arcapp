80174577bb2a6a6f6891c1db1541a84a171f7be2
igazoo <igazoo.23@gmail.com>
2019-11-29 14:06:43 +0900

    Add jquery

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get 'homes/index'
+  get 'homes/about'
+  get 'events/index'
   root 'homes#index'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
