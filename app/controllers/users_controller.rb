7199dae60a25ae6471f5bbd2fd1f6eca576fd0ec
igazoo <igazoo.23@gmail.com>
2020-01-20 17:37:56 +0900

    プロフィール画面とフォロワ画面のview編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,14 +45,14 @@ class UsersController < ApplicationController
    @title = "Following"
    @user  = User.find(params[:id])
    @users = @user.following
-   render 'show_follow'
+   render 'show_following'
  end
 
  def followers
    @title = "Followers"
    @user  = User.find(params[:id])
    @users = @user.followers
-   render 'show_follow'
+   render 'show_followers'
  end
 
 
